f9e4772c161e09cc698eb1d689db42d9c521f905
AkshanshKanojia <akshanshkanojia@gmail.com>
2023-02-24 17:27:39 +0530

    some things i forgot

--- Assets/Akshansh/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Akshansh/Scripts/Gameplay/Player/PlayerController.cs
@@ -9,12 +9,17 @@ namespace Gameplay.Player
 
         //serialized Fields
         [SerializeField] Transform playerCam;
-        [SerializeField] float moveSpeed = 2f, rotSenstivit = 3f, minMouseClamp = -40f, maxMouseClamp = 40f;
+        [SerializeField] bool canSprint = true;
+        [SerializeField] float moveSpeed = 2f, rotSenstivit = 3f, minMouseClamp = -40f, maxMouseClamp = 40f,sprintSpeed = 3f,
+            maxStaminaAvailable =10f,staminaConsumptionRate =1,staminaRestoreDelay = 2f,staminaRestoreRate =1;
+        [SerializeField] Animator playerAnim;
 
         //private fields
         PhotonView view;
         UIManager uiMang;
 
+        float tempSpeed;//used to store default speed
+        float curntStamina,staminaRestoreTimer =0;
         bool isActivePlayer = false, canRotateCam = true;
         Vector3 tempMoveDir;
         Vector2 tempMouseRot;
@@ -29,6 +34,8 @@ namespace Gameplay.Player
                 playerCam.gameObject.SetActive(true);
                 uiMang = FindObjectOfType<UIManager>();
                 uiMang.SetCursorVisibility(false);
+                tempSpeed = moveSpeed;
+                curntStamina = maxStaminaAvailable;
             }
         }
 
@@ -40,6 +47,10 @@ namespace Gameplay.Player
         private void Update()
         {
             InputHandler();
+            if(isActivePlayer)
+            {
+                RegenPropHandler();
+            }
         }
 
         /// <summary>
@@ -66,9 +77,53 @@ namespace Gameplay.Player
         /// <param name="_dir"></param>
         void MoveInDirection(Vector3 _dir)
         {
-            if (_dir != Vector3.zero)
+            if (_dir != Vector3.zero)//only if moving
             {
+                //sprint
+                if(Input.GetKey(KeyCode.LeftShift))
+                {
+                    if (curntStamina > 0)
+                    {
+                        //wait before restoring stamina
+                        staminaRestoreTimer = 0;
+                        moveSpeed = sprintSpeed;
+                        curntStamina -= Time.deltaTime * staminaConsumptionRate;
+                    }
+                    else
+                    {
+                        moveSpeed = tempSpeed;
+                    }
+                }
+                else if(moveSpeed==sprintSpeed)//if already sprinting
+                {
+                    moveSpeed = tempSpeed;
+                }
+
+                //apply movement
                 transform.Translate(moveSpeed * Time.deltaTime * _dir.normalized);
+                playerAnim.SetBool("IsWalking", true);
+            }
+            else
+            {
+                playerAnim.SetBool("IsWalking", false);
+            }
+        }
+
+        /// <summary>
+        /// controls properties which restore overtiem like mana and stamina.
+        /// </summary>
+        void RegenPropHandler()
+        {
+            if(staminaRestoreTimer<staminaRestoreDelay)
+            {
+                staminaRestoreTimer += Time.deltaTime;
+            }
+            else
+            {
+                if(curntStamina<maxStaminaAvailable)
+                {
+                    curntStamina += Time.deltaTime * staminaRestoreRate;
+                }
             }
         }
 
@@ -84,6 +139,18 @@ namespace Gameplay.Player
             //camera
             tempMouseRot.x = Mathf.Clamp(tempMouseRot.x, minMouseClamp, maxMouseClamp);
             playerCam.localEulerAngles = new Vector3(tempMouseRot.x, 0, 0);
+        }
+        #endregion
+
+        #region Public Functions
+        public float GetStamina()
+        {
+            return curntStamina;
+        }
+        public void AddToStamina(float _value)
+        {
+            curntStamina += _value;
+            curntStamina = Mathf.Clamp(curntStamina, 0, maxStaminaAvailable);
         }
         #endregion
     }
