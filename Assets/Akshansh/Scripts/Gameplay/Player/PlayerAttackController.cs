7e57f927653fc093f96c8e2bb1703ffaeb05696e
AkshanshKanojia <akshanshkanojia@gmail.com>
2023-03-09 10:11:36 +0530

    anim

---
+++ Assets/Akshansh/Scripts/Gameplay/Player/PlayerAttackController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Gameplay.Player
+{
+    public class PlayerAttackController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
